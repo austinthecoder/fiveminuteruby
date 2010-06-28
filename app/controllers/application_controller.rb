9e0201f7437ff066a697ddd7f609eb95b926c33b
Austin Schneider <soccer022483@gmail.com>
2010-06-28 00:16:13 -0400

    setup staging environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   
+  before_filter :authenticate, :if => lambda { Rails.env.staging? }
+  
   protect_from_forgery
   
   layout 'application'
   
   scintillate
+  
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "foo" && password == "bar"
+    end
+  end
   
 end

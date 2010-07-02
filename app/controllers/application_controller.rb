20213b760c5b97ad3c48d2823c89b2bedc2e4b5d
Austin Schneider <soccer022483@gmail.com>
2010-07-02 02:22:21 -0400

    in production, redirect to correct domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   
+  DOMAIN = 'www.fiveminuteruby.com'
+  
   include Fiveminuteruby::BasicAuth
   
   protect_from_forgery
   
   layout 'application'
   
+  before_filter :ensure_domain
+  
   scintillate
+  
+  private
+
+  def ensure_domain
+    redirect_to(DOMAIN) if Rails.env.production? && (request.env['HTTP_HOST'] != DOMAIN)
+  end
   
 end

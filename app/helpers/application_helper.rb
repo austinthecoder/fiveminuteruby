8a9b6b6d2655a465584db91ddf0ff80b9edc6f4c
Austin Schneider <soccer022483@gmail.com>
2010-07-06 22:37:29 -0400

    changed feed url to feedburner
    added license to about page
    
    changed some copy, styled.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     "FiveMinuteRuby"
   end
   
+  def feed_url
+    if Rails.env.production?
+      'http://feeds.feedburner.com/fiveminuteruby_screencasts'
+    else
+      screencasts_url(:format => :rss)
+    end
+  end
+  
 end
